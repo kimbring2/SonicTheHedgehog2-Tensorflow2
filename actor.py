0925aa5b2c66ddd6c30cb7ae20e8610c648907c5
Dohyeong Kim <kimbring2@gmail.com>
2022-05-11 04:37:03 +0900

    Add files via upload

--- actor.py
+++ actor.py
@@ -65,14 +65,15 @@ state_size = (64,64,3)
 stage_index = 0
 
 state_list = ['EmeraldHillZone.Act1', 'EmeraldHillZone.Act2'] 
+if arguments.env_id % 2 ==0:
+    env = retro.make(game='SonicTheHedgehog2-Genesis', scenario='contest', state='EmeraldHillZone.Act1')
+else:
+    env = retro.make(game='SonicTheHedgehog2-Genesis', scenario='contest', state='EmeraldHillZone.Act2')
 
 scores = []
 episodes = []
 average = []
 for episode_step in range(0, 2000000):
-    state = random.choice(state_list)
-    env = retro.make(game='SonicTheHedgehog2-Genesis', scenario='contest', state=state)
-
     observation = env.reset()
     observation_resized = cv2.resize(observation, dsize=(64,64), interpolation=cv2.INTER_AREA)
     state = observation_resized / 255.0
@@ -86,6 +87,7 @@ for episode_step in range(0, 2000000):
     reward = 0.0
     reward_sum = 0
     step = 0
+    last_screen_x = 0
     while True:
         try:
             step += 1
@@ -93,19 +95,34 @@ for episode_step in range(0, 2000000):
             state_reshaped = np.reshape(state, (1,*state_size)) 
 
             env_output = {"env_id": np.array([arguments.env_id]), 
-                          "reward": reward / 10.0,
+                          "reward": reward,
                           "done": done, 
                           "observation": state_reshaped}
             socket.send_pyobj(env_output)
             action_index = int(socket.recv_pyobj()['action'])
 
-            if arguments.env_id == 0:
+            if arguments.env_id == 0 or arguments.env_id == 1:
                 env.render()
 
             #print("action_index: ", action_index)
             action = possible_action_list[action_index]
 
-            observation1, reward, done, info = env.step(action)
+            observation1, _, done, info = env.step(action)
+            #print("info: ", info)
+
+            screen_x_end = info['screen_x_end']
+            screen_x = info['screen_x']
+
+            #if screen_x > last_screen_x * 0.5:
+            #    last_screen_x = screen_x
+            #    reward = 1.0
+            #    print("reward: ", reward)
+            if done == True:
+                reward = 1.0
+                print("reward: ", reward)
+            else:
+                reward = 0.0
+
             observation1 = cv2.cvtColor(observation1, cv2.COLOR_BGR2RGB)
             observation1_resized = cv2.resize(observation1, dsize=(64,64), interpolation=cv2.INTER_AREA)
             #observation1_resized = cv2.cvtColor(observation1_resized, cv2.COLOR_BGR2RGB)
@@ -117,7 +134,7 @@ for episode_step in range(0, 2000000):
 
             reward_sum += reward
             state = next_state
-            if done:
+            if done or step == 10000:
                 if arguments.env_id == 0:
                     scores.append(reward_sum)
                     episodes.append(episode_step)
@@ -132,6 +149,7 @@ for episode_step in range(0, 2000000):
                     print("reward_sum: " + str(reward_sum))
 
                 break
+
         except (tf.errors.UnavailableError, tf.errors.CancelledError):
             logging.info('Inference call failed. This is normal at the end of training.')
 
